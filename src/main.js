b915ab3b8ad6bbaab328b0bcb85d95cbf2f544b5
lanyulin <lanyulin@han-zi.cn>
2018-10-19 17:03:52 +0800

    优化一下

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//引ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import axios from 'axios';
+Vue.use(ElementUI,axios);
 
 Vue.config.productionTip = false
 
